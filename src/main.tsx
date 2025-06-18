32c4a548af62517974049870381f1b2fb3692757
ntt236 <nguyenthanhtrieu236@gmail.com>
2025-06-18 10:39:37 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import store from "./store/store.tsx";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/index.tsx";
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
