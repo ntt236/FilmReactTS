4d42e8ba166f17af35c6011179485db00c9f8890
ntt236 <nguyenthanhtrieu236@gmail.com>
2025-06-20 16:09:18 +0700

    update route & layout + component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,9 @@
+import MainLayout from "@/layouts/main/MainLayout";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    element: <MainLayout />,
   },
 ]);
